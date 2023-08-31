43da91bc08f3e397d719f22199767e21221ccf27
Crisppu <cristobalyat69@hotmail.com>
2023-08-30 18:12:14 -0600

    primera subida

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  // lo que esta comentado es porque e quitamos el modo estricto
+  // <React.StrictMode>
+    <App />
+  // </React.StrictMode>
+)
